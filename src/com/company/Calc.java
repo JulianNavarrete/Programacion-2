d3d73b7b8a57a7a4ec21f4ef7b405c98e035217a
JulianNavarrete <julyplottier@hotmail.com>
2022-03-16 20:30:28 -0300

    First class

---
+++ src/com/company/Calc.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Calc {
+
+    public static void showResult(int result) {
+        System.out.println("El resultado es: " + result);
+
+    }
+
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //System.out.println("Hi world");
+
+        Calc.showResult(Calc.add(3,3));
+    }
+
+}
